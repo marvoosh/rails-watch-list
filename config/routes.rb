868179c070cd408a5a571f364644ed4094d076a0
mn <marvoosh@gmail.com>
2021-11-11 16:35:42 +0000

    lists controller done, bookmark controller missing #destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'bookmarks/new'
+  get 'bookmarks/create'
+  get 'bookmarks/destroy'
+  get 'lists/index'
+  resources :lists do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: [:destroy]
+  # resources :movies, only: [:index]
 end
